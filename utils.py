1dd3b4269e1bac7f2eb77eca2a6f5a109a75f4d7
Naved Hashmi <navedhashmi33@gmail.com>
2022-06-14 02:12:50 +0530

    Oauth Finalisation and Redirect implementation

--- utils.py
+++ utils.py
@@ -1,12 +1,22 @@
-from pydoc import plain
+from fastapi.responses import RedirectResponse
 from passlib.context import CryptContext
 
 pass_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+#Password Hashing
 def hash(password: str):
     return pass_context.hash(password)
 
+#Password Verification
 def verify(plain_password, hashed_password):
     return pass_context.verify(plain_password, hashed_password)
+
+#Template Rendering
+def render()
+
+#Redirect Responses
+#def redirect(path, cookies:dict={}):
+    #response = RedirectResponse(path, status_code=302)
+
 
 
