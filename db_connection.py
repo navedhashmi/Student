b046c3770e634bdb58b5bfa61a2c94e7ebf52290
Naved Hashmi <navedhashmi33@gmail.com>
2022-07-17 17:34:39 +0530

    Changes on static file loading cmd in html

--- db_connection.py
+++ db_connection.py
@@ -3,12 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 from config import get_settings
+import urllib, os
 
 # Instance of Enviorment Variables
 settings = get_settings()
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}"
+ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
 
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}?sslmode={ssl_mode}"
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SeassionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
